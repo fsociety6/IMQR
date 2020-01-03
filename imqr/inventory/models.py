c62afca858934f399bd902a9e6736009a8424c12
Shubham Sanjiv Naik <naikshub8412@gmail.com>
2020-01-03 11:46:51 +0530

    created models for items and category

--- imqr/inventory/models.py
+++ imqr/inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	category=models.CharField(max_length=50)
+	def __str__(self):
+		return self.category		
+
+class Item(models.Model):
+	item_id=models.AutoField(primary_key=True)
+	name=models.CharField(max_length=50)
+	category=models.ForeignKey(Category,on_delete=models.CASCADE)
+	quantity=models.IntegerField(default=0)
+	def __str__(self):
+		return self.name
+
+
+
